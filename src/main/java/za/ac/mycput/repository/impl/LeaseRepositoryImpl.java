04fe6efee6ef3e587715b6eedbcb220633fdacdc
gitboimal22 <230388175@mycput.ac.za>
2026-03-27 22:24:26 +0200

    Update LeaseRepositoryImpl.java

--- src/main/java/za/ac/mycput/repository/impl/LeaseRepositoryImpl.java
+++ src/main/java/za/ac/mycput/repository/impl/LeaseRepositoryImpl.java
@@ -1,5 +1,13 @@
 package za.ac.mycput.repository.impl;
 
+
+/*
+* Lease.java
+* Lease for the tenant
+* Malik Muhammed (230388175)
+* 21/3/2026
+ */
+
 import za.ac.mycput.domain.Lease;
 import za.ac.mycput.repository.IRepository;
 import java.util.ArrayList;
